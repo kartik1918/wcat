b44ad5c48b6139ff1f605fbbf6a8217c2171d06d
Kartik Gupta <kartikgupta966@gmail.com>
2021-08-11 15:26:42 +0530

    Node Filesystem

--- fs/fs.js
+++ fs/fs.js
@@ -6,7 +6,7 @@ console.log(fs.existsSync("a.txt"));
 
 console.log(fs.existsSync("b.txt"));
 
-fs.writeFileSync("a.txt", "how are you?"); //first it will delete the all data and write the line
+fs.writeFileSync("a.txt", "how are you?"); //first it will delete all the data and then write the line
 
 fs.writeFileSync("b.txt", "Something Test");
 
@@ -15,4 +15,7 @@ console.log(process.argv); // passing arguments in command line
 let file1name = process.argv[2];
 let file2name = process.argv[4];
 
-let file1data = 
+let file1data = fs.readFileSync("file1name", "utf-8");
+let file2data = fs.readFileSync("file2name", "utf-8");
+
+console.log()
